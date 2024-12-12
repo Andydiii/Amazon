88d4c0b4c21b18b6f2a58a9d2f22d58f2d8c283d
Andydiii <andydiii0813@gmail.com>
2024-12-11 19:33:51 -0500

    completed half less part 2

--- less10-html+css+js/scripts/10i-j.js
+++
@@ -1,9 +0,0 @@
-let calculation = localStorage.getItem("calculation") || "";
-
-updateResult("");
-
-function updateResult(operation) {
-    calculation += operation;
-    document.querySelector('.shownumber').innerHTML = calculation;
-    localStorage.setItem("calculation", calculation);
-}
