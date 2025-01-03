c38aef8b55c2d26390221867560cf97198c67410
Andydiii <andydiii0813@gmail.com>
2025-01-02 23:50:53 -0500

    did nothing

--- less13-amazon-project/data/cart.js
+++ less13-amazon-project/data/cart.js
@@ -4,6 +4,7 @@ export let cart = JSON.parse(localStorage.getItem('cart')) || [{
 }, {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1
+    
 }];
 
 // calculate the quantity when we clicked addToCart button
