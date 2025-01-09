0916622d696f7dd0441c349d998944eefa50e002
Andydiii <Andydiii0813@gmail.com>
2025-01-09 16:12:21 -0500

    removed default tests and add money tests

--- less13-amazon-project/tests/moneyTest.js
+++ less13-amazon-project/tests/moneyTest.js
@@ -1,5 +1,8 @@
 import { formatCurrency } from "../scripts/utils/money.js";
 
+console.log('test Suite: formatCurrency');
+
+console.log('converts cents into dollars');
 
 if (formatCurrency(2095) === '20.95') {
     console.log('passed');
