200603e856c231851a21f62f6614a447e570aaa0
Andydiii <Andydiii0813@gmail.com>
2025-01-11 12:58:45 -0500

    fix the bug for automated testing

--- less13-amazon-project/data/cart.js
+++ less13-amazon-project/data/cart.js
@@ -1,23 +1,36 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) || [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
-}, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-}];
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem('cart')) || [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+        deliveryOptionId: '1'
+    }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionId: '2'
+    }];
+}
 
 // calculate the quantity when we clicked addToCart button
 export function addToCart(productId) {
     let existedInCart;
   
     // quantity = 1 by default
-    const quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
-    
+    const ele = document.querySelector(`.js-quantity-selector-${productId}`)
+    let quantity;
+    // to make automating testing working. otherwise query selector will return null.
+    if (ele === null) {
+        quantity = 1;
+    } else {
+        quantity = Number(ele.value);
+    }
+
     cart.forEach((cartItem) => {
         if (cartItem.productId === productId) {
-            cartItem.quantity += quantity;
+            cartItem.quantity += quantity; 
             existedInCart = true;
         }     
     });
--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -1,5 +1,19 @@
-import { addToCart } from "../../less13-amazon-project/data/cart.js";
+import { addToCart, cart , loadFromStorage} from "../../less13-amazon-project/data/cart.js";
 
 describe('test suite: addToCart', () => {
-    
+    it('adds an existing product to the cart', () => {
+
+    });
+
+    it('adds a new product into the cart', () => {
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([]);
+        });
+        
+        console.log(localStorage.getItem('cart'));
+        loadFromStorage();
+
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart.length).toEqual(1);
+    });
 });
