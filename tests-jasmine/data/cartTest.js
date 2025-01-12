697c2974941c8a0d32e029b3e7818e2759727d34
Andydiii <Andydiii0813@gmail.com>
2025-01-12 00:23:42 -0500

    updated

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -6,14 +6,20 @@ describe('test suite: addToCart', () => {
     });
 
     it('adds a new product into the cart', () => {
+
+        // we dont want testing to change the localstorage, so we make fake code for setItem
+        spyOn(localStorage, 'setItem');
+
+        // ensure we test the code based on fixed situation which is cart is always empty.
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([]);
         });
-        
-        console.log(localStorage.getItem('cart'));
         loadFromStorage();
 
         addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+
         expect(cart.length).toEqual(1);
+        // each test can have multiple expect and it will only pass if all expect passed
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     });
 });
