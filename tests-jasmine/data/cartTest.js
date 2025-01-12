673c9a3dd614c647eab3670ad4f6fdc37100874b
Andydiii <Andydiii0813@gmail.com>
2025-01-12 15:46:08 -0500

    created automated tests for addToCart

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -2,7 +2,28 @@ import { addToCart, cart , loadFromStorage} from "../../less13-amazon-project/da
 
 describe('test suite: addToCart', () => {
     it('adds an existing product to the cart', () => {
+        // we dont want testing to change the localstorage, so we make fake code for setItem
+        // spyOn has feature that records everytime a function is used.
+        spyOn(localStorage, 'setItem');
 
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([{
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 1,
+                deliveryOptionId: '1'
+            }]);
+        });
+        loadFromStorage();
+
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+
+        expect(cart.length).toEqual(1);
+        // each test can have multiple expect and it will only pass if all expect passed
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart[0].quantity).toEqual(2);
+
+        
     });
 
     it('adds a new product into the cart', () => {
@@ -21,5 +42,7 @@ describe('test suite: addToCart', () => {
         expect(cart.length).toEqual(1);
         // each test can have multiple expect and it will only pass if all expect passed
         expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart[0].quantity).toEqual(1);
     });
 });
