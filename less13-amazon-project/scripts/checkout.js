2b63816e90e341d661b603ea724770624a6d74a2
Andydiii <Andydiii0813@gmail.com>
2025-01-19 01:17:16 -0500

    half done with js lesson17 practice. Continue on 17f

--- less13-amazon-project/scripts/checkout.js
+++ less13-amazon-project/scripts/checkout.js
@@ -1,6 +1,44 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+
+// for 17a
+import { Car, newToyota, newTesla } from "../../less17-OOP/practices/data/car.js";
+
 // import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
+
+
+// for 17a
+console.log(newToyota);
+console.log(newTesla);
+
+// 17b
+newToyota.displayInfo();
+newTesla.displayInfo();
+
+
+// 17c
+newToyota.go();
+newToyota.go();
+newToyota.displayInfo();
+newToyota.reset();
+
+newTesla.go();
+newTesla.brake();
+newTesla.displayInfo();
+newTesla.reset();
+
+// 17d
+newToyota.displayInfo();
+newTesla.displayInfo();
+
+newToyota.openTrunk();
+newToyota.displayInfo();
+
+newToyota.go();
+newToyota.closeTrunk();
+newToyota.go();
+newToyota.displayInfo();
+newToyota.openTrunk();
---
+++ less17-OOP/practices/data/car.js
@@ -0,0 +1,89 @@
+// 17a
+export class Car {
+    brand;
+    model;
+    trunkOpened = false;
+    speed = 0;
+
+    constructor(carObj) {
+        this.brand = carObj.brand;
+        this.model = carObj.model;
+    }
+
+    displayInfo() {
+        console.log(`brand: ${this.brand}, model: ${this.model}, speed: ${this.speed} km/h, trunk status: ${this.trunkOpened ? 'Opened' : 'Closed'}`);
+    }
+
+    go() {
+        if (this.trunkOpened === false && this.speed + 5 <= 200) {
+            this.speed += 5;
+        } else if (this.trunkOpened) {
+            console.log('Your trunck is still open, dont drive!');
+        } else {
+            console.log('Your speed is too fast');
+        }
+    }
+    
+    brake() {
+        if (this.speed - 5 >= 0) {
+            this.speed -= 5;
+        }
+    }
+
+    openTrunk() {
+        if (this.speed === 0) {
+            this.trunkOpened = true;
+        } else {
+            console.log("you are driving! You cannot open Trunk");
+        }
+    }
+
+    closeTrunk() {
+        this.trunkOpened = false;
+    }
+    
+    reset() {
+        this.speed = 0;
+        this.trunkOpened = false;
+    }
+}
+
+export const newToyota = new Car({
+    brand: 'Toyota',
+    model: 'Corolla'
+})
+
+export const newTesla = new Car({
+    brand:'Tesla',
+    model: 'Model 3'
+})
+
+
+export class RaceCar extends Car{
+    acceleration;
+
+    constructor(raceCarObj) {
+        super(raceCarObj);
+        this.acceleration = raceCarObj.acceleration;
+    }
+
+    go() {
+        if (this.speed + this.acceleration > 300) {
+            this.speed = 300;
+        }
+    }
+
+    openTrunk() {
+        console.log('Race car do not have a trunk');
+    }
+
+    closeTrunk() {
+        console.log('Race car do not have a trunk');
+    }
+}
+
+const Porche = new RaceCar({
+    brand: 'porche',
+    model: 'Cayne',
+    acceleration: 15
+});
